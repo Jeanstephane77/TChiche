c8c02930e34d1c108ce24d5e17ebdc0a78d99a0c
Jeanstephane77 <jskouadio@edu.oreegami.com>
2025-08-30 22:05:35 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'tchiche-cache-v1';
+const CACHE_NAME = 'tchiche-cache-v2'; // J'ai changé la version du cache
 const urlsToCache = [
   '/',
-  'index.html',
-  'script.js',
-  'manifest.json',
-  'icons/icon-192.png',
-  'icons/icon-512.png',
+  '/index.html',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Montserrat:wght@400;600&display=swap'
 ];
 
@@ -29,6 +29,22 @@ self.addEventListener('fetch', event => {
         return fetch(event.request); // Fetch from network
       }
     )
+  );
+});
+
+// Supprimer les anciens caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
